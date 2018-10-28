0bcd6adf24661015454fcfa94bc455009ec09f62
Jyothi <jyothi1138@gmail.com>
2018-10-28 16:09:02 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def homepage():
+    return "Hello world"
+
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
+
